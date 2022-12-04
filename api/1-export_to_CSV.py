e80a9fd67b5c43aa17a68c15632ae3acbca2ed20
MichaelTwG <michaelgolenia1@gmail.com>
2022-12-04 15:40:02 -0800

    1-export_to_CSV.py WIP; git push; clear

---
+++ api/1-export_to_CSV.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+""" module gather_data_from_an_API """
+
+import requests
+from sys import argv
+
+
+if __name__ == "__main__":
+    "root of API: https://jsonplaceholder.typicode.com/"
+
+    u_id = argv[1]
+    url_r = "https://jsonplaceholder.typicode.com/{}"
+
+    u_request = requests.get(url_r.format("users/" + u_id)).json()
+    to_do_r = requests.get(url_r.format("todos/")).json()
+
+    to_do_usr = [to for to in to_do_r if to.get("userId") == int(u_id)]
+
+    u_name = u_request.get("name")
+    file_name = "{}.csv".format(u_id)
+    csv = ""
+    for task in to_do_usr:
+        task_status = to_do_usr.get("completed")
+        task_title = to_do_usr.get("title")
+
+        text = '"{}","{}","{}","{}"'.format(u_id,
+                                    u_name,
+                                    task_status,
+                                    task_title)
+        csv += text + "\n"
+    with open(file_name, "w", encoding="utf-8") as csv_file:
+        csv_file.write(csv)
