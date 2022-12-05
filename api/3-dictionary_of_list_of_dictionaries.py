4fa81a6f45d9443ab44d210e712d43977c71fc4a
Your Name <michaelgolenia1@gmail.com>
2022-12-04 21:53:13 -0300

    3-dictionary_of_list_of_dictionaries.py

--- api/3-dictionary_of_list_of_dictionaries.py
+++ api/3-dictionary_of_list_of_dictionaries.py
@@ -10,24 +10,24 @@ if __name__ == "__main__":
     "root of API: https://jsonplaceholder.typicode.com/"
 
     dict_result = {}
-    u_id = argv[1]
     url_r = "https://jsonplaceholder.typicode.com/{}"
 
-    u_request = requests.get(url_r.format("users/" + u_id)).json()
+    u_request = requests.get(url_r.format("users/")).json()
     to_do_r = requests.get(url_r.format("todos/")).json()
 
-    dict_result[u_id] = []
-    file_name = "{}.json".format(u_id)
+    file_name = "todo_all_employees.json"
 
-    u_name = u_request.get("username")
-
-    for task in to_do_r:
-        dict_task = {}
-        if task.get("userId") == int(u_id):
-            dict_task["task"] = task.get("title")
-            dict_task["completed"] = task.get("completed")
-            dict_task["username"] = u_name
-            dict_result[u_id].append(dict_task)
+    for user in u_request:
+        u_id = user.get("id")
+        dict_result[u_id] = []
+        u_name = user.get("username")
+        for task in to_do_r:
+            dict_task = {}
+            if task.get("userId") == int(u_id):
+                dict_task["task"] = task.get("title")
+                dict_task["completed"] = task.get("completed")
+                dict_task["username"] = u_name
+                dict_result[u_id].append(dict_task)
 
     with open(file_name, "w", encoding="utf-8") as json_file:
         json.dump(dict_result, json_file)
